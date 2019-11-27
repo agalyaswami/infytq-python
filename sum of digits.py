2d864aa7019084bafa794d8c85e1be979c62249b
agalyaswami <53601736+agalyaswami@users.noreply.github.com>
2019-11-26 19:42:41 -1000

    Create sum of digits.py

---
+++ sum of digits.py
@@ -0,0 +1,14 @@
+
+def find_sum_of_digits(number):
+    sum_of_digits=0
+    dig=0
+    while(number>0):
+        dig=number%10
+        sum_of_digits=sum_of_digits+dig
+        number=number//10
+    return sum_of_digits
+
+#Provide different values for number and test your program
+sum_of_digits=find_sum_of_digits(123)
+print("Sum of digits:",sum_of_digits)
+                                          
